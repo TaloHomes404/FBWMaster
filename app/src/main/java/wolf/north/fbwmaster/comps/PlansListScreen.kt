6944f4d7bed9d82d1d1521bc8ba693893143402d
Bartek <bartlomiej00wilk@gmail.com>
2024-09-30 15:06:50 +0200

    comp init

--- app/src/main/java/wolf/north/fbwmaster/comps/PlansListScreen.kt
+++ app/src/main/java/wolf/north/fbwmaster/comps/PlansListScreen.kt
@@ -1,9 +1,6 @@
 package wolf.north.fbwmaster.comps
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -17,18 +14,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Calculate
 import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FormatListNumbered
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.PlaylistAddCheck
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
@@ -36,11 +31,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -50,18 +43,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import wolf.north.fbwmaster.R
-import wolf.north.fbwmaster.comps.ui.theme.FBWMasterTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun PlansListScreen() {
     Scaffold(
         topBar = {
             Column {
@@ -116,22 +107,39 @@ fun MainScreen() {
                     .padding(16.dp)
             ) {
                 // Nagłówek sekcji "Workouts"
-                Text(
-                    text = "Workouts",
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Bold,
-                    letterSpacing = 1.sp,
-                    color = Color.Black
-                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Select ready-to-go plan!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                }
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Karta
-                WorkoutCard(
-                    imageResId = R.drawable.arms_fbw1, // Zdjęcie treningu
-                    title = "Full Body Mobility Routine",
-                    duration = "36 mins"
-                )
+                // Lista kart w LazyColumn
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(16.dp) // Odstęp między kartami
+                ) {
+                    items(2) { index ->
+                        PlanCard(
+                            imageResId = R.drawable.arms_fbw1, // Zdjęcie treningu
+                            title = "Full Body Mobility Routine",
+                            duration = "36 mins"
+                        )
+                    }
+                }
             }
         },
         bottomBar = {
@@ -175,10 +183,12 @@ fun MainScreen() {
 }
 
 @Composable
-fun WorkoutCard(imageResId: Int, title: String, duration: String) {
+fun PlanCard(imageResId: Int, title: String, duration: String) {
     Card(
         shape = RoundedCornerShape(16.dp),
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth() // Karta wypełnia pełną szerokość
+            .wrapContentHeight() // Ustalony rozmiar dla kart
     ) {
         Box {
             // Tło ze zdjęciem
@@ -187,7 +197,8 @@ fun WorkoutCard(imageResId: Int, title: String, duration: String) {
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(275.dp).alpha(0.8f),
+                    .height(180.dp)
+                    .alpha(0.8f),
                 contentScale = ContentScale.Crop
             )
 
@@ -243,6 +254,6 @@ fun WorkoutCard(imageResId: Int, title: String, duration: String) {
 
 @Preview(showSystemUi = true)
 @Composable
-private fun MainScreenPreview() {
-    MainScreen()
+private fun PlanListPreview() {
+    PlansListScreen()
 }
